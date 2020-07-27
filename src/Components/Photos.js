e45871bbff278426aa5a1dcc1d976afa08922c21
malee1 <46596915+malee1@users.noreply.github.com>
2020-07-27 17:12:25 +0100

    first commit

---
+++ src/Components/Photos.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles'
+import { Grid, Card, CardContent, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles({
+   card: {
+       backgroundColor: 'lightgrey'
+   }
+
+});
+
+export default function Photos() {
+
+  const classes = useStyles();
+
+    const [photos, setPhotos] = useState([]);
+
+    const getImages = async() => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+        const json = await response.json();
+        setPhotos(json);
+    }
+
+    useEffect(() => {
+        getImages()
+    }, []);
+
+    return (
+
+      <div>
+        <h3>Placeholder Images</h3>
+        <Grid container spacing={3}>
+          {photos.map((photo) => (
+            <Grid item xs={3} key={photo.id}>
+              <Card className={classes.card}>
+                <CardContent className="cardContent">
+                  <img key={photo.id} src={photo.thumbnailUrl} alt=""></img>
+                  <Typography>Name: {photo.title}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+
+     
+      </div>
+    );
+
+   
+}
---
+++ src/Components/Users.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Grid, Card, CardContent, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+   card: {
+       backgroundColor: 'lightgrey'
+   }
+
+});
+
+export default function Users() {
+
+    const classes = useStyles();
+
+    const [users, setUsers] = useState([]);
+
+    const getUsers = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const json = await response.json();
+        setUsers(json);
+    }
+
+   useEffect(() => { getUsers() }, []);
+
+    return (
+      <div>
+        <h3>List of all Users</h3>
+        <Grid container spacing={3}>
+          {users.map(user => (
+            <Grid item xs={3} key={user.id}>
+              <Card className={classes.card}>
+                 <CardContent className="cardContent">
+                <Typography>Name: {user.name}</Typography>
+              </CardContent>
+              <CardContent className="cardContent">
+                <Typography>User Name: {user.username}</Typography>
+              </CardContent>
+              <CardContent className="cardContent">
+                <Typography>email: {user.email}</Typography>
+              </CardContent>
+              <CardContent className="cardContent">
+                <Typography>Website: {user.website}</Typography>
+              </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    );
+}
